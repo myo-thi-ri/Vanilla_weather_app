8d6a239d8a8b545f565d749ab5ca5370ddeb4f7f
myo_thi_ri <sandarlamin.llm@gmail.com>
2021-05-12 18:01:34 +0900

    Added search engine

--- src/app.js
+++ src/app.js
@@ -58,3 +58,12 @@ function displayTemperature(response) {
 }
 
 axios.get(apiUrl).then(displayTemperature);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
